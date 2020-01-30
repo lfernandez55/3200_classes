41149ba5ac20634437771e8ae8e004aa413cf6e8
lfernandez55 <lfernandez@weber.edu>
2020-01-29 18:26:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from vehicles import Car, ElectricCar
+
+ferrari = Car("Ferrari", "200")
+ferrari.run_car()
+
+leaf = ElectricCar("Leaf", "70", "all electric")
+leaf.run_car()
+leaf.display_electric_type()
+
+
